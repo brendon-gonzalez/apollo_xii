28a259a73aecd44a6f316888a3887ede17a0c55d
Brendon Gonzalez <bmgonzalezmm@gmail.com>
2017-03-03 14:45:59 -0500

    Fresh start

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-var $ = require('jquery');
-var Backbone = require('backbone');
-var React = require('react');
-
-var Router = require('./router');
-var router = new Router();
-
-Backbone.$ = $;
-Backbone.history.start();
