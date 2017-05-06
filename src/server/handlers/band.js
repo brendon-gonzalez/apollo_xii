2f190b5f0a83bfdfe6defc0cd5dc702cf571eae9
Brendon Gonzalez <bmgonzalezmm@gmail.com>
2017-05-06 03:13:40 -0400

    Add album parsing

--- src/server/handlers/band.js
+++ src/server/handlers/band.js
@@ -25,10 +25,43 @@ export function index(req, res) {
   request({ url, timeout: config.timeout })
   .then((body) => {
     const $ = cheerio.load(body);
+    const albumTypes = $('.plaincontentbox > tr span[style*="a23939"]')
+      .map((i, section) => ({
+        start: $('.plaincontentbox > tr').index($(section).closest('tr').get(0)),
+        type: $(section).text()
+      })).get();
+
+    const parsedAlbums = albumTypes.map((albumType, albumTypeIndex) => {
+      const albums = [];
+      const next = albumTypes[albumTypeIndex + 1];
+      const start = albumType.start + 1;
+      const end = (next) ? next.start - 1 : $('.plaincontentbox > tr').length - 1;
+
+      for (let i = start; i <= end; i += 1) {
+        const albumsInRow = $('.plaincontentbox > tr').eq(i).find('td[width="120"]');
+        albumsInRow.each((j, albumInRow) => {
+          const albumStats = $(albumInRow).next('td');
+          const album = {
+            image: $(albumInRow).find('img').attr('src'),
+            title: albumStats.find('font[size="2"]').text(),
+            year: parseInt(albumStats.find('font[size="1"]').text().split('/').pop(), 10),
+            rating: parseFloat(albumStats.find('center font[size="4"]').text(), 10),
+            votes: parseInt(albumStats.find('center font[size="1"]').text().replace(',', ''), 10)
+          };
+          albums.push(album);
+        });
+      }
+
+      return {
+        type: albumType.type,
+        albums
+      };
+    });
+
     const response = {
       name: $('font[size="6"]').text(),
       genres: $('.tags .tag').map((i, tag) => $(tag).text().trim()).get(),
-      albums: Array(7),
+      albums: parsedAlbums,
       similar_bands: $('p.alt2[style*="font-size:9pt"] > a').map((i, band) => ({
         name: $(band).text(),
         href: $(band).attr('href')
--- test/server/band.js
+++ test/server/band.js
@@ -11,7 +11,7 @@ describe('band', () => {
           expect(res, 'status').to.have.status(200);
           expect(body.name, 'name').to.eq('Mastodon');
           expect(body.similar_bands).to.be.a('array');
-          expect(body.similar_bands).to.have.length(5);
+          expect(body.similar_bands).to.have.length(4);
           expect(body.genres).to.be.a('array');
           expect(body.genres).to.have.length(3);
           expect(body.albums).to.be.a('array');
@@ -32,6 +32,91 @@ describe('band', () => {
           ];
           expect(genres).to.be.a('array');
           expect(genres).to.eql(expectedGenres);
+          done();
+        });
+    });
+
+    it('has the correct albums', (done) => {
+      chai.request(server)
+        .get('/band/Mastodon')
+        .end((err, res) => {
+          const { albums } = res.body;
+          const LPs = albums[0];
+          const expectedLPs = [{
+            image: '/images/albums/243422.jpg-thumbl',
+            title: 'Emperor of Sand',
+            year: 2017,
+            rating: 3.8,
+            votes: 654
+          },
+          { image: '/images/albums/163355.jpg-thumbl',
+            title: 'Once More \'Round the Sun',
+            year: 2014,
+            rating: 3.7,
+            votes: 1591
+          },
+          { image: '/images/albums/83301.jpg-thumbl',
+            title: 'The Hunter',
+            year: 2011,
+            rating: 3.7,
+            votes: 2465
+          },
+          { image: '/images/albums/36170.jpg-thumbl',
+            title: 'Crack the Skye',
+            year: 2009,
+            rating: 4.1,
+            votes: 3916
+          },
+          { image: '/images/albums/11289.jpg-thumbl',
+            title: 'Blood Mountain',
+            year: 2006,
+            rating: 4,
+            votes: 3658
+          },
+          { image: '/images/albums/1474.jpg-thumbl',
+            title: 'Leviathan',
+            year: 2004,
+            rating: 4.2,
+            votes: 3840
+          },
+          { image: '/images/albums/982.jpg-thumbl',
+            title: 'Remission',
+            year: 2002,
+            rating: 3.8,
+            votes: 2162
+          }];
+          expect(albums).to.be.a('array');
+          expect(albums).to.have.length(4);
+          expect(LPs.type).to.eq('LPs');
+          expect(LPs.albums).to.have.length(7);
+          expect(LPs.albums).to.eql(expectedLPs);
+          done();
+        });
+    });
+
+    it('has the correct similar artists', (done) => {
+      chai.request(server)
+        .get('/band/Mastodon')
+        .end((err, res) => {
+          const { similar_bands } = res.body;
+          const expectedSimilarBands = [{
+            href: '/band/Intronaut',
+            name: 'Intronaut'
+          },
+          {
+            href: '/band/Baroness',
+            name: 'Baroness'
+          },
+          {
+            href: '/band/Red+Fang',
+            name: 'Red Fang'
+          },
+          {
+            href: '/band/Burst',
+            name: 'Burst'
+          }];
+          expect(similar_bands).to.be.a('array');
+          expect(similar_bands).to.eql(expectedSimilarBands);
           done();
         });
     });
